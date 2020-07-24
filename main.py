ba837e5c3d770ae9e6f74413d1d75c39212598ea
sigit wasis subekti <sigitghoticmetal2001@gmail.com>
2020-07-24 15:00:20 +0700

    best practice python using pycharm

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 
 print('Hello World!')
 try:
-    r = requests.get('https://goog le.com')
+    r = requests.get('https://google.com')
     print(r.status_code)
     # Jika status 200 maka berhasil dan tampilkan text dari url google.com
     if r.status_code == 200:
